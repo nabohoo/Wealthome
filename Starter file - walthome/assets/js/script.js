561408e379df4077a867bbc913b1ec1f4b08fe2f
nabohoo <159925844+nabohoo@users.noreply.github.com>
2026-04-05 23:32:47 +0700

    Add files via upload

---
+++ Starter file - walthome/assets/js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// nacber toggle in mobile
+
+const $navbar = document.querySelector('[data-navbar]');
+const $navToggler = document.querySelector('[data-nav-toggler]');
+
+$navToggler.addEventListener('click', () => $navbar.classList.toggle('active'));
+
+
+// header scroll state
+
+const $header= document.querySelector('[data-header]');
+window.addEventListener('scroll', e => {
+    $header.classList[window.scrollY > 50  ? 'add' : 'remove']('active')});
+
+// add to favorite button toggle
+
+const $toggleBtns= document.querySelectorAll('[data-toggle-btn]')
+
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener('click', () => {
+        $toggleBtn.classList.toggle('active');
+    });
+});
